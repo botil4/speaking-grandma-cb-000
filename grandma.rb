22ce319c7a055fca76b89900346fa465c3d8aefe
Travis Clarkson <tk1667@gmail.com>
2018-02-19 00:21:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    (phrase == "I LOVE YOU GRANDMA!") ? ("I LOVE YOU TOO PUMPKIN!") : ("NO, NOT SINCE 1938!")
+    phrase == "I LOVE YOU GRANDMA!" ? "I LOVE YOU TOO PUMPKIN!" : "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
